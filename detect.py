6275651c075c7bff3453b10f8a34353f5ff5020c
Tu Ngo <tungo.ee.96@gmail.com>
2019-07-15 23:33:52 +0700

    file for executing the detection

---
+++ detect.py
@@ -0,0 +1,170 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 15 21:41:07 2019
+
+@author: tungo
+"""
+
+from __future__ import division
+import time
+import torch 
+import torch.nn as nn
+import numpy as np
+import cv2 
+from util import *
+import argparse
+import os 
+import os.path as osp
+from darknet import Net
+import pickle as pkl
+import pandas as pd
+import random
+
+def arg_parse():
+    """
+    Parse arguements to the detect module
+    
+    """
+    
+    parser = argparse.ArgumentParser(description='YOLO v3 Detection Module')
+   
+    parser.add_argument("--images", dest = 'images', help = 
+                        "Image / Directory containing images to perform detection upon",
+                        default = "imgs", type = str)
+    parser.add_argument("--det", dest = 'det', help = 
+                        "Image / Directory to store detections to",
+                        default = "det", type = str)
+    parser.add_argument("--bs", dest = "bs", help = "Batch size", default = 1)
+    parser.add_argument("--confidence", dest = "confidence", help = "Object Confidence to filter predictions", default = 0.5)
+    parser.add_argument("--nms_thresh", dest = "nms_thresh", help = "NMS Threshhold", default = 0.4)
+    parser.add_argument("--cfg", dest = 'cfgfile', help = 
+                        "Config file",
+                        default = "darknet/cfg/yolov3.cfg", type = str)
+    parser.add_argument("--weights", dest = 'weightsfile', help = 
+                        "weightsfile",
+                        default = "yolov3.weights", type = str)
+    parser.add_argument("--reso", dest = 'reso', help = 
+                        "Input resolution of the network. Increase to increase accuracy. Decrease to increase speed",
+                        default = "416", type = str)
+    
+    return parser.parse_args()
+
+def load_classes(filename):
+    file = open(filename, 'r')
+    lines = file.read().split('\n')[:-1]
+    return lines
+
+def letterbox_image(img, inp_dim):
+    '''resize image with unchanged aspect ratio using padding'''
+    img_w, img_h = img.shape[1], img.shape[0]
+    w, h = inp_dim
+    new_w = int(img_w * min(w/img_w, h/img_h))
+    new_h = int(img_h * min(w/img_w, h/img_h))
+    resized_image = cv2.resize(img, (new_w,new_h), interpolation = cv2.INTER_CUBIC)
+    
+    canvas = np.full((inp_dim[1], inp_dim[0], 3), 128)
+
+    canvas[(h-new_h)//2:(h-new_h)//2 + new_h,(w-new_w)//2:(w-new_w)//2 + new_w,  :] = resized_image
+    
+    return canvas
+
+def prep_image(img, inp_dim):
+    """
+    Prepare image for inputting to the neural network. 
+    
+    Returns a Variable 
+    """
+
+    img = cv2.resize(img, (inp_dim, inp_dim))
+    img = img[:,:,::-1].transpose((2,0,1)).copy()
+    img = torch.from_numpy(img).float().div(255.0).unsqueeze(0)
+    return img
+    
+args = arg_parse()
+images = args.images
+batch_size = int(args.bs)
+confidence = float(args.confidence)
+nms_thesh = float(args.nms_thresh)
+start = 0
+
+classes = load_classes('datasets/coco.names')
+
+#Set up the neural network
+print("Loading network.....")
+model = Net(args.cfgfile)
+model.load_weights(args.weightsfile)
+print("Network successfully loaded")
+
+model.net_info["height"] = args.reso
+inp_dim = int(model.net_info["height"])
+assert inp_dim % 32 == 0 
+assert inp_dim > 32
+model.eval()
+
+try:
+    imlist = [osp.join(osp.realpath('.'), images, img) for img in os.listdir(images)]
+except NotADirectoryError:
+    imlist = []
+    imlist.append(osp.join(osp.realpath('.'), images))
+except FileNotFoundError:
+    print ("No file or directory with the name {}".format(images))
+    exit()
+    
+if not os.path.exists(args.det):
+    os.makedirs(args.det)
+    
+loaded_ims = [cv2.imread(x) for x in imlist]
+
+#PyTorch Variables for images
+im_batches = list(map(prep_image, loaded_ims, [inp_dim for x in range(len(imlist))]))
+
+#List containing dimensions of original images
+im_dim_list = [(x.shape[1], x.shape[0]) for x in loaded_ims]
+im_dim_list = torch.FloatTensor(im_dim_list).repeat(1,2)
+
+leftover = 0
+if (len(im_dim_list) % batch_size):
+   leftover = 1
+
+if batch_size != 1:
+   num_batches = len(imlist) // batch_size + leftover            
+   im_batches = [torch.cat((im_batches[i*batch_size : min((i +  1)*batch_size,
+                       len(im_batches))]))  for i in range(num_batches)]  
+
+write = 0
+start_det_loop = time.time()
+pred = model(im_batches[0])
+for i, batch in enumerate(im_batches):
+    #load the image 
+    start = time.time()
+
+    prediction = model(batch)
+
+    prediction = write_results(prediction, confidence, iou_thresh = nms_thesh)
+
+    end = time.time()
+
+    if type(prediction) == int:
+
+        for im_num, image in enumerate(imlist[i*batch_size: min((i +  1)*batch_size, len(imlist))]):
+            im_id = i*batch_size + im_num
+            print("{0:20s} predicted in {1:6.3f} seconds".format(image.split("/")[-1], (end - start)/batch_size))
+            print("{0:20s} {1:s}".format("Objects Detected:", ""))
+            print("----------------------------------------------------------")
+        continue
+
+    prediction[:,0] += i*batch_size    #transform the atribute from index in batch to index in imlist 
+
+    if not write:                      #If we have't initialised output
+        output = prediction  
+        write = 1
+    else:
+        output = torch.cat((output,prediction))
+
+    for im_num, image in enumerate(imlist[i*batch_size: min((i +  1)*batch_size, len(imlist))]):
+        im_id = i*batch_size + im_num
+        objs = [classes[int(x[-1])] for x in output if int(x[0]) == im_id]
+        print("{0:20s} predicted in {1:6.3f} seconds".format(image.split("/")[-1], (end - start)/batch_size))
+        print("{0:20s} {1:s}".format("Objects Detected:", " ".join(objs)))
+        print("----------------------------------------------------------")
+        
