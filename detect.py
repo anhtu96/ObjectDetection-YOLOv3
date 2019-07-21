35a89475574043589464bfaa35d2bd532add4801
Tu Ngo <tungo.ee.96@gmail.com>
2019-07-21 23:19:12 +0700

    draw bounding boxes, save images in to det folder

--- detect.py
+++ detect.py
@@ -131,9 +131,9 @@ if (len(im_dim_list) % batch_size):
    leftover = 1
 
 if batch_size != 1:
-   num_batches = len(imlist) // batch_size + leftover            
-   im_batches = [torch.cat((im_batches[i*batch_size : min((i +  1)*batch_size,
-                       len(im_batches))]))  for i in range(num_batches)]  
+    num_batches = len(imlist) // batch_size + leftover            
+    im_batches = [torch.cat((im_batches[i*batch_size : min((i + 1)*batch_size,
+                           len(im_batches))]))  for i in range(num_batches)]  
 
 write = 0
 start_det_loop = time.time()
@@ -173,4 +173,43 @@ for i, batch in enumerate(im_batches):
         print("{0:20s} {1:s}".format("Objects Detected:", " ".join(objs)))
         print("----------------------------------------------------------")
         
-torch.cuda.empty_cache()
+# create bounding boxes
+try:
+    output
+except NameError:
+    print ("No detections were made")
+    exit()
+    
+im_dim_list = torch.index_select(im_dim_list, 0, output[:,0].long())
+scaling_factor = torch.min(inp_dim/im_dim_list,1)[0].view(-1,1)
+output[:,[1,3]] -= (inp_dim - scaling_factor*im_dim_list[:,0].view(-1,1))/2
+output[:,[2,4]] -= (inp_dim - scaling_factor*im_dim_list[:,1].view(-1,1))/2
+output[:,1:5] /= scaling_factor
+
+for i in range(output.shape[0]):
+    output[i, [1,3]] = torch.clamp(output[i, [1,3]], 0.0, im_dim_list[i,0])
+    output[i, [2,4]] = torch.clamp(output[i, [2,4]], 0.0, im_dim_list[i,1])
+    
+class_load = time.time()
+colors = pkl.load(open("pallete", "rb"))
+# draw boxes
+draw = time.time()
+
+def write(x, results):
+    c1 = tuple(x[1:3].int())
+    c2 = tuple(x[3:5].int())
+    img = results[int(x[0])]
+    cls = int(x[-1])
+    color = random.choice(colors)
+    label = "{0}".format(classes[cls])
+    cv2.rectangle(img, c1, c2,color, 1)
+    t_size = cv2.getTextSize(label, cv2.FONT_HERSHEY_PLAIN, 1 , 1)[0]
+    c2 = c1[0] + t_size[0] + 3, c1[1] + t_size[1] + 4
+    cv2.rectangle(img, c1, c2,color, -1)
+    cv2.putText(img, label, (c1[0], c1[1] + t_size[1] + 4), cv2.FONT_HERSHEY_PLAIN, 1, [225,255,255], 1);
+    return img
+
+list(map(lambda x: write(x, loaded_ims), output))
+det_names = pd.Series(imlist).apply(lambda x: "{}/det_{}".format(args.det,x.split("\\")[-1]))
+list(map(cv2.imwrite, det_names, loaded_ims))
+end = time.time()
